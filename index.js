b407270a778bfcf17e8afcdd60025c1473a619e9
Kibet Kipsang <denniskipsang2000@gmail.com>
2024-11-27 20:46:48 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1=31
+const num2=2
+const multiply = (num1*num2);
+console.log (multiply);
 
-// Write your code here
+const num3=16
+const num4=6
+const mod = (num3%num4);
+console.log(mod);
+
+const max = Math.max(5,10,15,20);
+console.log(max);
+
+const random =Math.ceil(Math.random()+5);
+console.log(random);
